1d7313c82f61d316d071ec622b67e6cb48759363
Khorunzhiy Ilya <khorunzhiy.ib@gmail.com>
2023-12-18 23:13:42 +0300

    add flask app

--- app/main.py
+++ app/main.py
@@ -1,33 +1,10 @@
-import os
-from time import sleep
+from flask import Flask
 
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, text
+app = Flask(__name__)
 
-db_name = os.getenv('db_name', 'database')
-db_user = os.getenv('db_ueser', 'postgres')
-db_password = os.getenv('db_password', 'postgres')
-db_host = os.getenv('db_host', 'db')
-db_port = os.getenv('db_port', '5432')
+@app.route('/')
+def hello():
+    return 'Hello, Flask!'
 
 if __name__ == '__main__':
-    print('Application started')
-    db_string = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(db_user, db_password, db_host, db_port, db_name)
-    engine = create_engine(db_string)
-    metadata = MetaData()
-    books = Table(
-        'book', metadata,
-        Column('id', Integer, primary_key=True),
-        Column('title', String),
-        Column('author', String),
-    )
-
-    metadata.create_all(engine)
-
-    with engine.connect() as con:
-        statement = text("""INSERT INTO book (id, title, author) VALUES (1, 'The Hobbit', 'Tolkien')""")
-        con.execute(statement)
-
-        rs = con.execute(text('SELECT * FROM book'))
-
-        for row in rs:
-            print(row)
+    app.run(debug=True, host='0.0.0.0', port=8000)
